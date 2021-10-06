0fb1dead74a42e2747867b757dd5ae5628f3c29c
Juanjo Jaramillo <juanjodev02@MacBook-Pro-de-Juanjo.local>
2021-10-05 20:32:04 -0500

    finish model implementation

--- controllers/Auth.controller.js
+++ controllers/Auth.controller.js
@@ -3,11 +3,25 @@ const router = express.Router();
 
 const Console = require('../lib/Console');
 const Response = require('../lib/Response');
+const AuthService = require('../services/Auth.service');
 
 const console = new Console('AUTH-CONTROLLER');
 const response = new Response();
 
-router.post('/user', () => {});
+router.post('/user', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return response.error(res, 'Email and password are required');
+        }
+        const authService = await AuthService.getInstance();
+        const isLoggedIn = await authService.userLogin(email, password);
+        response.success(res, isLoggedIn);
+    } catch(error) {
+        console.error(error);
+        response.error(res, error.message);
+    }
+});
 
 router.post('/administrator', () => {})
 
--- services/Auth.service.js
+++ services/Auth.service.js
@@ -1,3 +1,47 @@
-class AuthService {}
+const Database = require('../lib/Database');
+const bcrypt = require('bcrypt');
+
+class AuthService {
+    static _authServiceInstance = null;
+
+    async getModels() {
+        const { UserModel, AuthModel, AdministratorModel } = await Database.getModels();
+        this._userModel = UserModel;
+        this._authModel = AuthModel;
+        this._administratorModel = AdministratorModel;
+    }
+
+    static async getInstance() {
+        if (AuthService._authServiceInstance === null) {
+            AuthService._authServiceInstance = new AuthService();
+            await AuthService._authServiceInstance.getModels();
+        }
+        return AuthService._authServiceInstance;
+    }
+
+    async userLogin(email, password) {
+        const user = await this._userModel.findOne({
+            where: {
+                email,
+            },
+            include: [{
+                model: this._authModel,
+            }]
+        });
+        if (!user) {
+            throw new Error('Credentials are not valid');
+        }
+        const auth = await user.getAuth();
+        const isPasswordValid = await bcrypt.compare(password, auth.password);
+        if (!isPasswordValid) {
+            throw new Error('Credentials are not valid');
+        }
+        return true;
+    }
+
+    async adminLogin(email, password) {
+        
+    }
+}
 
 module.exports = AuthService;
